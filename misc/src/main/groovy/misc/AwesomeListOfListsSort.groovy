5fed019a55c7e6e24ea8429905e81c505a8cb6bb
Adam Skucinski <askucins@gmail.com>
2018-11-26 00:24:35 +0100

    fixed bug in sorting code

--- misc/src/main/groovy/misc/AwesomeListOfListsSort.groovy
+++ misc/src/main/groovy/misc/AwesomeListOfListsSort.groovy
@@ -3,6 +3,7 @@ package misc
 /*
 An awesome trick to sort list of lists (not necessarily of a homogeneous lengths)
 From https://stackoverflow.com/questions/1467641/groovy-list-sort-by-first-second-then-third-elements/30443821#30443821
+However there is a bug there...
  */
 
 class AwesomeListOfListsSort {
@@ -10,7 +11,7 @@ class AwesomeListOfListsSort {
         lol.sort { l1, l2 ->
             def (e1, e2) = [l1, l2].transpose().find { e1, e2 ->
                 e1 != e2
-            }
+            } ?: [null, null]
             e1 <=> e2
         }
     }
--- misc/src/test/groovy/misc/AwesomeListOfListsSortSpec.groovy
+++ misc/src/test/groovy/misc/AwesomeListOfListsSortSpec.groovy
@@ -9,10 +9,14 @@ class AwesomeListOfListsSortSpec extends Specification {
     def "should sort list of lists"() {
         expect:
         verifyAll {
+            sortListOfLists([]) == []
             sortListOfLists([[]]) == [[]]
             sortListOfLists([[2, 1, 3], [2, 0, 5], [3, 1]]) == [[2, 0, 5], [2, 1, 3], [3, 1]]
             sortListOfLists([[2, 1, 3], [2, 0, 5], [1, 5]]) == [[1, 5], [2, 0, 5], [2, 1, 3]]
             sortListOfLists([[3], [2, 1], [1, 2, 3]]) == [[1, 2, 3], [2, 1], [3]]
+            sortListOfLists([[2, 2], [2, 1, 3], [2, 1, 2]]) == [[2, 1, 2], [2, 1, 3], [2, 2]]
+            sortListOfLists([[1, 1], [2, 1, 3], [2, 1, 2]]) == [[1, 1], [2, 1, 2], [2, 1, 3]]
+            sortListOfLists([[1, 1], [1, 1]]) == [[1, 1], [1, 1]]
         }
     }
 }
