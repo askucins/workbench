ad222d7461baa9afe48f33d81cbad5af5de91fae
Adam Skucinski <askucins@gmail.com>
2020-01-18 05:06:36 +0100

    added an info about running via intellij with ivy added explicitly

--- scripts/GebSmoke.groovy
+++ scripts/GebSmoke.groovy
@@ -1,3 +1,5 @@
+// To run in Intellij add this module/jar as a dependency: org.apache.ivy:ivy:[version]
+// To get it installed: grape install org.apache.ivy ivy 2.5.0
 @Grapes([
         @Grab("org.gebish:geb-core:3.2"),
         //@Grab("org.seleniumhq.selenium:selenium-firefox-driver:4.0.0-alpha-3"),
