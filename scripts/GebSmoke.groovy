128a1078bf04206f619f2074243f85c91ab90e87
Adam Skucinski <askucins@gmail.com>
2020-02-23 23:03:16 +0100

    added scaffolding for stuff from Grokking Algorithms book

--- scripts/GebSmoke.groovy
+++ scripts/GebSmoke.groovy
@@ -1,10 +1,10 @@
 // To run in Intellij add this module/jar as a dependency: org.apache.ivy:ivy:[version]
 // To get it installed: grape install org.apache.ivy ivy 2.5.0
 @Grapes([
-        @Grab("org.gebish:geb-core:3.2"),
-        //@Grab("org.seleniumhq.selenium:selenium-firefox-driver:4.0.0-alpha-3"),
-        @Grab("org.seleniumhq.selenium:selenium-chrome-driver:4.0.0-alpha-3"),
-        @Grab("org.seleniumhq.selenium:selenium-support:4.0.0-alpha-3"),
+        @Grab("org.gebish:geb-core:3.3"),
+        //@Grab("org.seleniumhq.selenium:selenium-firefox-driver:4.0.0-alpha-4"),
+        @Grab("org.seleniumhq.selenium:selenium-chrome-driver:4.0.0-alpha-4"),
+        @Grab("org.seleniumhq.selenium:selenium-support:4.0.0-alpha-4"),
         @GrabExclude("org.codehaus.groovy:groovy-xml"),
         @GrabExclude("org.codehaus.groovy:groovy-json"),
         @GrabExclude("org.codehaus.groovy:groovy-swing"),
@@ -19,6 +19,6 @@ import geb.Browser
 
 Browser.drive {
     go "http://gebish.org"
-    assert title == "Geb - Very Groovy Browser AutomationX"
+    assert title == "Geb - Very Groovy Browser Automation"
 }
 
