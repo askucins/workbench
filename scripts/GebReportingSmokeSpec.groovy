314100290b54ccabec48417113858d4ae2a9f448
Adam Skucinski <askucins@gmail.com>
2020-08-26 20:15:22 +0200

    bumped versions of libraries

--- scripts/GebReportingSmokeSpec.groovy
+++ scripts/GebReportingSmokeSpec.groovy
@@ -1,10 +1,18 @@
+// - Grapes vs Intellij
 // To run in Intellij add this module/jar as a dependency: org.apache.ivy:ivy:[version]
+// e.g. /home/askuci/.groovy/grapes/org.apache.ivy/ivy/jars/ivy-2.5.0.jar
 // To get it installed: grape install org.apache.ivy ivy 2.5.0
+//
+// - Groovy 3.0.4 vs 3.0.5 command line
+// 3.0.4 is the last version when it works from a command line "groovy GebSmoke.groovy" from SDKMAN's groovy...
 
 @Grapes([
-    //@Grab("org.seleniumhq.selenium:selenium-firefox-driver:4.0.0-alpha-5"),
-    @Grab("org.seleniumhq.selenium:selenium-chrome-driver:4.0.0-alpha-5"),
-    @Grab("org.seleniumhq.selenium:selenium-support:4.0.0-alpha-5"),
+    @Grab("org.gebish:geb-core:3.4"),
+    @Grab("org.gebish:geb-spock:3.4"),
+    @Grab("org.spockframework:spock-core:2.0-M3-groovy-3.0"),
+    @Grab("org.spockframework:spock-junit4:2.0-M3-groovy-3.0"),
+    @Grab("org.seleniumhq.selenium:selenium-chrome-driver:4.0.0-alpha-6"),
+    //@Grab("org.seleniumhq.selenium:selenium-firefox-driver:4.0.0-alpha-6"),
     @GrabExclude("org.codehaus.groovy:groovy-xml"),
     @GrabExclude("org.codehaus.groovy:groovy-json"),
     @GrabExclude("org.codehaus.groovy:groovy-swing"),
@@ -13,10 +21,6 @@
     @GrabExclude("org.codehaus.groovy:groovy-macro"),
     @GrabExclude("org.codehaus.groovy:groovy-nio"),
     @GrabExclude("org.codehaus.groovy:groovy-sql"),
-    @Grab("org.spockframework:spock-core:2.0-M2-groovy-3.0"),
-    @Grab("org.spockframework:spock-junit4:2.0-M2-groovy-3.0"),
-    @Grab("org.gebish:geb-core:3.4"),
-    @Grab("org.gebish:geb-spock:3.4")
 ])
 
 import geb.Browser
@@ -28,7 +32,8 @@ import spock.util.environment.RestoreSystemProperties
 class GebReportingSmokeSpec extends GebReportingSpec {
 
     def setupSpec() {
-        System.setProperty('webdriver.chrome.driver', '/home/askuci/.gradle/webdriver/chromedriver/81.0.4044.138/chromedriver_linux64/cw2ad4f0dy1a6ngf4tmo5n56p/chromedriver')
+        System.setProperty('webdriver.chrome.driver', '/home/askuci/.gradle/webdriver/chromedriver/85.0.4183.38/chromedriver_linux64/6h50nn75cxnis2g4ls5ycnvgk/chromedriver')
+        System.setProperty('webdriver.gecko.driver', '/home/askuci/.gradle/webdriver/geckodriver/0.26.0/geckodriver-v0.26.0-linux64.tar/d873k1x1c8078m6sc0ysb8ijy/geckodriver')
     }
 
     def setup() {
@@ -36,7 +41,7 @@ class GebReportingSmokeSpec extends GebReportingSpec {
         CachingDriverFactory.clearCacheAndQuitDriver()
     }
 
-    def "smoke test"() {
+    def "should pass this test"() {
         expect:
         Browser.drive {
             go "https://gebish.org"
@@ -46,7 +51,7 @@ class GebReportingSmokeSpec extends GebReportingSpec {
         println 'Done!'
     }
 
-    def "smoke test 2"() {
+    def "should fail this test"() {
         expect:
         Browser.drive {
             go "https://gebish.org"
--- scripts/GebSmoke.groovy
+++ scripts/GebSmoke.groovy
@@ -1,10 +1,15 @@
+// - Grapes vs Intellij
 // To run in Intellij add this module/jar as a dependency: org.apache.ivy:ivy:[version]
+// e.g. /home/askuci/.groovy/grapes/org.apache.ivy/ivy/jars/ivy-2.5.0.jar
 // To get it installed: grape install org.apache.ivy ivy 2.5.0
+//
+// - Groovy 3.0.4 vs 3.0.5 command line
+// 3.0.4 is the last version when it works from a command line "groovy GebSmoke.groovy" from SDKMAN's groovy...
+
 @Grapes([
     @Grab("org.gebish:geb-core:3.4"),
-    //@Grab("org.seleniumhq.selenium:selenium-firefox-driver:4.0.0-alpha-5"),
-    @Grab("org.seleniumhq.selenium:selenium-chrome-driver:4.0.0-alpha-5"),
-    @Grab("org.seleniumhq.selenium:selenium-support:4.0.0-alpha-5"),
+    @Grab("org.seleniumhq.selenium:selenium-chrome-driver:4.0.0-alpha-6"),
+    //@Grab("org.seleniumhq.selenium:selenium-firefox-driver:4.0.0-alpha-6"),
     @GrabExclude("org.codehaus.groovy:groovy-xml"),
     @GrabExclude("org.codehaus.groovy:groovy-json"),
     @GrabExclude("org.codehaus.groovy:groovy-swing"),
@@ -17,13 +22,15 @@
 
 import geb.Browser
 
-System.setProperty('webdriver.chrome.driver', '/home/askuci/.gradle/webdriver/chromedriver/81.0.4044.138/chromedriver_linux64/cw2ad4f0dy1a6ngf4tmo5n56p/chromedriver')
+System.setProperty('webdriver.chrome.driver', '/home/askuci/.gradle/webdriver/chromedriver/85.0.4183.38/chromedriver_linux64/6h50nn75cxnis2g4ls5ycnvgk/chromedriver')
+System.setProperty('webdriver.gecko.driver', '/home/askuci/.gradle/webdriver/geckodriver/0.26.0/geckodriver-v0.26.0-linux64.tar/d873k1x1c8078m6sc0ysb8ijy/geckodriver')
 
 Browser.drive {
     go "https://gebish.org"
     assert title == "Geb - Very Groovy Browser Automation"
 }.quit()
 
-System.setProperty('webdriver.chrome.driver', null)
+System.clearProperty('webdriver.chrome.driver')
+System.clearProperty('webdriver.gecko.driver')
 
 println 'Done!'
