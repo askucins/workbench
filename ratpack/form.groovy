00b21a164e8fb165caca0c416a37346a978a551f
Adam Skucinski <askucins@gmail.com>
2020-10-17 16:01:24 +0200

    added form example - init

---
+++ ratpack/form.groovy
@@ -0,0 +1,10 @@
+@Grab('io.ratpack:ratpack-groovy:1.8.0')
+@GrabExclude('org.codehaus.groovy:groovy-all')
+@Grab('org.slf4j:slf4j-simple:1.7.30')
+
+import static ratpack.groovy.Groovy.ratpack
+
+ratpack {	
+	handlers {
+	}
+}
