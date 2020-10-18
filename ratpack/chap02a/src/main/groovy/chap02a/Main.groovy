15de50156336d34049c61df04450dd51943452fe
Adam Skucinski <askucins@gmail.com>
2020-10-18 17:51:09 +0200

    made that minimal example working with groovy 3.x

---
+++ ratpack/chap02a/src/main/groovy/chap02a/Main.groovy
@@ -0,0 +1,18 @@
+package chap02a
+
+import ratpack.groovy.Groovy 
+import ratpack.server.RatpackServer 
+
+
+class Main {
+    static void main(String... args) {
+        RatpackServer.start { spec ->
+            spec.handlers(Groovy.chain {
+                get {
+                    render "Hello, world!"
+                }
+            })
+        }        
+    }
+}
+
