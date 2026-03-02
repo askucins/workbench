37dedc7f06d1c5fcea780c50aec1e18cce244738
Adam Skucinski <askucins@gmail.com>
2026-03-02 07:26:18 +0100

    bumped deps

--- geb-bare-bone/src/test/groovy/ProxyWhenChromeSpec.groovy
+++ geb-bare-bone/src/test/groovy/ProxyWhenChromeSpec.groovy
@@ -1,7 +1,7 @@
 import groovy.util.logging.Slf4j
 import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.devtools.v144.network.Network
-import org.openqa.selenium.devtools.v144.storage.Storage
+import org.openqa.selenium.devtools.v145.network.Network
+import org.openqa.selenium.devtools.v145.storage.Storage
 import org.openqa.selenium.logging.LogType
 import utils.ProxyCustomAuto
 
